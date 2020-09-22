7d6f43bb82a8a2da378bdd6204917d0772a7542a
Jabal Torres <info@jabaltorres.com>
2020-09-22 07:34:39 -0700

    Merge pull request #7 from capitalJT/projects_template_update
    Made projects present in grid view

--- wp-content/themes/ftf-altitude-pro/includes/featured_projects_cpt.php
+++ wp-content/themes/ftf-altitude-pro/includes/featured_projects_cpt.php
@@ -1,31 +1,22 @@
 <?php
     $project_client = get_field('project_client');
-//    $project_url = get_field('project_url');
-//    $project_button_text = get_field('project_button_text');
+    $project_url = get_field('project_url');
+    $project_button_text = get_field('project_button_text');
+//    $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+
+    $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'medium' );
 ?>
 
-<article class="featured-project-list-item border has-hover p-4 mb-4">
-  
-    <a href="<?php echo get_permalink(); ?>"><h1 class="article-title"><?php echo the_title();?></h1></a>
-
-	<?php if ( has_post_thumbnail() ) : ?>
-        <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" class="fp-thumbnail">
-            <img class="thumbnail border mb-2" src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_post_thumbnail_caption(); ?>" />
+<div class="featured-project-list-item mb-4 col-12 col-md-4">
+    <div class="border p-4 has-hover project-card" style="background-image: url('<?php echo $backgroundImg[0]; ?>');">
+	    <a href="<?php echo get_permalink(); ?>" class="article-title-wrapper">
+            <h3 class="article-title text-center"><?php echo the_title();?></h3>
         </a>
-	<?php endif; ?>
 
-
-	<?php if ($project_client) : ?>
-        <div class="project-title h4">Client: <?php echo $project_client;?></div>
-	<?php endif; ?>
-
-    <div class="project-excerpt"><?php the_excerpt(); ?></div>
-
-	<?php if ($project_url) : ?>
-        <a class="project-url btn border" href="<?php echo $project_url;?>">
+        <a class="project-url btn border" href="<?php echo get_permalink(); ?>">
             <?php echo $project_button_text;?>
         </a>
-	<?php endif; ?>
+    </div>
 
   <?php echo edit_post_link('(Edit)', '<span>', '</span>'); ?>
-</article>
+</div>
