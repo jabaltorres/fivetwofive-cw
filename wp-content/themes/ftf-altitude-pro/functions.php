1a14a1a9c401f613f5fbb39e8845fae7c7658ab3
capitalJT <info@jabaltorres.com>
2021-02-09 08:21:38 -0800

    Added template column to Pages backend

--- wp-content/themes/ftf-altitude-pro/functions.php
+++ wp-content/themes/ftf-altitude-pro/functions.php
@@ -463,7 +463,7 @@ function my_login_stylesheet() {
 add_action( 'login_enqueue_scripts', 'my_login_stylesheet' );
 
 
-// Add custom post type to main query
+// Add Featured Projects custom post type to main query
 add_action('pre_get_posts', 'query_post_type');
 function query_post_type($query) {
 	if($query->is_main_query()
@@ -498,3 +498,28 @@ function google_tag_manager_body() { ?>
     <!-- End Google Tag Manager (noscript) -->
 
 <?php }
+
+
+/**
+ * Custom Column with Currently Active Page Template - JT
+ * https://www.isitwp.com/custom-column-with-currently-active-page-template/
+ */
+add_filter( 'manage_pages_columns', 'page_column_views' );
+add_action( 'manage_pages_custom_column', 'page_custom_column_views', 5, 2 );
+function page_column_views( $defaults ) {
+    $defaults['page-layout'] = __('Template');
+    return $defaults;
+}
+function page_custom_column_views( $column_name, $id ) {
+    if ( $column_name === 'page-layout' ) {
+        $set_template = get_post_meta( get_the_ID(), '_wp_page_template', true );
+        if ( $set_template == 'default' ) {
+            echo 'Default';
+        }
+        $templates = get_page_templates();
+        ksort( $templates );
+        foreach ( array_keys( $templates ) as $template ) :
+            if ( $set_template == $templates[$template] ) echo $template;
+        endforeach;
+    }
+}
