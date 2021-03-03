ed8270a76d7b9950f3f29b0090616aa49ef7ae28
Danilo Parra Jr <daniloparrajr.work@gmail.com>
2021-03-03 21:51:38 +0800

    Added Testimonial ACF block

---
+++ wp-content/themes/genesis-fivetwofive/lib/acf/init.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * ACF Blocks
+ *
+ * @package Genesis FiveTwoFive
+ * @author  Danilo Parra Jr.
+ * @license GPL-2.0-or-later
+ * @link    https://daniloparrajr.com/
+ */
+
+add_action('acf/init', 'genesis_fivetwofive_register_acf_blocks');
+/**
+ * Register ACF blocks.
+ *
+ * @return void
+ */
+function genesis_fivetwofive_register_acf_blocks() {
+
+    // Check function exists.
+    if ( function_exists('acf_register_block_type') ) {
+        // Register a Featured Project block.
+        acf_register_block_type(array(
+            'name'              => 'testimonial',
+            'title'             => __('Testimonial'),
+            'description'       => __('Display Testimonial'),
+            'render_template'   => 'lib/acf/blocks/testimonial/testimonial.php',
+			'enqueue_style'     => get_stylesheet_directory_uri() . '/lib/acf/blocks/testimonial/testimonial.css',
+            'category'          => 'formatting',
+        ));
+    }
+
+}
