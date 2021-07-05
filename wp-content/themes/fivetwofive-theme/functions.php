a0fb6d76b0be53c8901820a5898e3903a46ebef1
Danilo Parra Jr <daniloparrajr.work@gmail.com>
2021-07-05 09:49:22 +0800

    Update FiveTwoFive Theme version

--- wp-content/themes/fivetwofive-theme/functions.php
+++ wp-content/themes/fivetwofive-theme/functions.php
@@ -9,7 +9,7 @@
 
 if ( ! defined( 'FIVETWOFIVE_THEME_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( 'FIVETWOFIVE_THEME_VERSION', '0.3.8' );
+	define( 'FIVETWOFIVE_THEME_VERSION', '0.4.0' );
 }
 
 /**
