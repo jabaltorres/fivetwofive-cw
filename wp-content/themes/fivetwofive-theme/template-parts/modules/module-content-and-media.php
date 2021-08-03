f48f316da4f8b6c5c6de05408ae1af6ca5ecc6fd
Danilo Parra Jr <daniloparrajr.work@gmail.com>
2021-08-03 12:49:04 +0800

    Add animation on all modules

--- wp-content/themes/fivetwofive-theme/template-parts/modules/module-content-and-media.php
+++ wp-content/themes/fivetwofive-theme/template-parts/modules/module-content-and-media.php
@@ -53,8 +53,51 @@ if ( $text_color ) {
 	$text_color_inline_style = sprintf( 'color:%1$s;', $text_color );
 }
 
+// Animations.
+$module_id                 = uniqid( 'ftf-module-content-and-media' );
+$module_animation_desktop  = get_sub_field( 'animation_desktop' );
+$module_animation_mobile   = get_sub_field( 'animation_mobile' );
+$module_animation_reset    = get_sub_field( 'animation_reset' );
+$module_animation_delay    = get_sub_field( 'animation_delay' );
+$module_animation_distance = get_sub_field( 'animation_distance' );
+$module_animation_duration = get_sub_field( 'animation_duration' );
+$module_animation_opacity  = get_sub_field( 'animation_opacity' );
+$module_animation_origin   = get_sub_field( 'animation_origin' );
+$module_animation_scale    = get_sub_field( 'animation_scale' );
+
+$module_animation_options = array(
+	'reset'   => $module_animation_reset,
+	'origin'  => $module_animation_origin,
+	'desktop' => $module_animation_desktop,
+	'mobile'  => $module_animation_mobile,
+);
+
+if ( $module_animation_delay ) {
+	$module_animation_options['delay'] = $module_animation_delay;
+}
+
+if ( $module_animation_distance ) {
+	$module_animation_options['distance'] = $module_animation_distance . 'px';
+}
+
+if ( $module_animation_duration ) {
+	$module_animation_options['duration'] = (int) $module_animation_duration;
+}
+
+if ( $module_animation_opacity ) {
+	$module_animation_options['opacity'] = (int) $module_animation_opacity;
+}
+
+if ( $module_animation_scale ) {
+	$module_animation_options['scale'] = (int) $module_animation_scale;
+}
+
+if ( $module_animation_desktop || $module_animation_mobile ) {
+	$module_classes .= ' ftf-module-hidden';
+}
+
 ?>
-<section class="ftf-module ftf-module-content-and-media py-5 py-md-6 text-md-<?php echo esc_attr( $text_alignment ); ?> <?php echo esc_attr( $module_classes ); ?>" style="<?php echo esc_attr( $styles ); ?>">
+<section class="ftf-module ftf-module-content-and-media py-5 py-md-6 text-md-<?php echo esc_attr( $text_alignment ); ?> <?php echo esc_attr( $module_classes ); ?>" id="<?php echo esc_attr( $module_id ); ?>" data-animation="<?php echo esc_attr( wp_json_encode( $module_animation_options ) ); ?>" style="<?php echo esc_attr( $styles ); ?>">
 	<div class="container">
 		<div class="row">
 			<?php if ( $module_image || $module_video ) : ?>
