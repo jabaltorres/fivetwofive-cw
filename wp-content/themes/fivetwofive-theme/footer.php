bcc757db190b80dff42c6252f2ecdd1df7b506d5
capitalJT <info@jabaltorres.com>
2021-10-02 14:54:32 -0700

    Added business options and theme options. Displayed custom logo in footer.

--- wp-content/themes/fivetwofive-theme/footer.php
+++ wp-content/themes/fivetwofive-theme/footer.php
@@ -15,12 +15,26 @@
 	<?php do_action( 'fivetwofive_before_footer' ); ?>
 	<footer id="colophon" class="site-footer">
 		<div class="container site-footer__bottom">
-			<p class="site-footer__copyright">
+
+            <?php
+            $custom_logo_id = get_theme_mod( 'custom_logo' );
+            $custom_logo_image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+            $theme_logo = $custom_logo_image[0];
+            ?>
+
+            <img src="<?= $theme_logo; ?>" alt="Theme Logo">
+
+            <?php if (get_field('business_name', 'option')): ?>
+                <div class="site-footer__business_name"><?php the_field('business_name', 'option'); ?></div><!-- .site-footer__business_name -->
+            <?php endif; ?>
+
+            <p class="site-footer__copyright">
 				<?php
 				/* translators: %1$s: Copyright year, %2$s: Site name */
 				echo sprintf( esc_html__( 'Copyright © %1$s. %2$s. All rights reserved.', 'fivetwofive' ), esc_html( wp_date( 'Y' ) ), esc_html( get_bloginfo( 'name' ) ) );
 				?>
-			</p><!-- .site-footer__copyright -->
+            </p><!-- .site-footer__copyright -->
+
 		</div><!-- .site-footer__bottom -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
