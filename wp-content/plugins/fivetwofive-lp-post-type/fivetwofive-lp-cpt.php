abc941117347ddb12a9110ec7cfcc425c71cb3b1
Danilo Parra Jr <daniloparrajr.work@gmail.com>
2021-10-15 14:30:18 +0800

    feat: hide the content editor if the landing page use modules template

--- wp-content/plugins/fivetwofive-lp-post-type/fivetwofive-lp-cpt.php
+++ wp-content/plugins/fivetwofive-lp-post-type/fivetwofive-lp-cpt.php
@@ -113,7 +113,53 @@ register_activation_hook( __FILE__, 'ftf_setup_fivetwofive_lp_custom_post_type' 
  * @return void
  */
 function ftf_unregister_fivetwofive_lp_custom_post_type() {
-    unregister_post_type( 'fivetwofive-lp' );
-    flush_rewrite_rules();
+	unregister_post_type( 'fivetwofive-lp' );
+	flush_rewrite_rules();
 }
 register_deactivation_hook( __FILE__, 'ftf_unregister_fivetwofive_lp_custom_post_type' );
+
+/**
+ * Don't support content editor if modules template is used in landing pages.
+ *
+ * @link https://developer.wordpress.org/reference/functions/remove_post_type_support/
+ * @return void
+ */
+function ftf_remove_editor_init() {
+    // If not in the admin, return.
+    if ( ! is_admin() ) {
+       return;
+    }
+ 
+    // Get the post ID on edit post with filter_input super global inspection.
+    $current_post_id = filter_input( INPUT_GET, 'post', FILTER_SANITIZE_NUMBER_INT );
+    // Get the post ID on update post with filter_input super global inspection.
+    $update_post_id = filter_input( INPUT_POST, 'post_ID', FILTER_SANITIZE_NUMBER_INT );
+ 
+    // Check to see if the post ID is set, else return.
+    if ( isset( $current_post_id ) ) {
+       $post_id = absint( $current_post_id );
+    } else if ( isset( $update_post_id ) ) {
+       $post_id = absint( $update_post_id );
+    } else {
+       return;
+    }
+ 
+    // Don't do anything unless there is a post_id.
+    if ( ! isset( $post_id ) ) {
+		return;
+    }
+
+	// make sure the post type is correct.
+	if ( 'fivetwofive-lp' !== get_post_type( $post_id ) ) {
+		return;
+	}
+
+	// Get the template of the current post.
+	$template_file = get_post_meta( $post_id, '_wp_page_template', true );
+
+	// Example of removing page editor for page-templates/template-module.php template.
+	if (  'page-templates/template-module.php' === $template_file ) {
+		remove_post_type_support( 'fivetwofive-lp', 'editor' );
+	}
+}
+add_action( 'init', 'ftf_remove_editor_init' );
