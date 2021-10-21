9d0f34d3c910f1e58226e17638316bdfaa91d8fd
Danilo Parra Jr <daniloparrajr.work@gmail.com>
2021-10-21 08:53:56 +0800

    refactor: change the fivetwofive plugins author to fivetwofive creative team

--- wp-content/plugins/fivetwofive-lp-post-type/fivetwofive-lp-cpt.php
+++ wp-content/plugins/fivetwofive-lp-post-type/fivetwofive-lp-cpt.php
@@ -1,15 +1,17 @@
 <?php
 /**
- * Plugin Name: FiveTwoFive LP Custom Post Type
+ * Plugin Name: FiveTwoFive - Landing Page Custom Post Type
+ * Plugin URI: https://fivetwofive.com/
  * Description: A simple plug in that adds an lp custom post type
  * Version: 0.1
- * Author:  Jabal Torres
+ * Author: FiveTwoFive Creative Team
+ * Author URI: https://fivetwofive.com/
  * License: GPL2
  * Text Domain: ftf-lps
  */
 
 /*
-	Copyright 2020: Jabal Torres
+	Copyright 2020: FiveTwoFive Creative Team
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
