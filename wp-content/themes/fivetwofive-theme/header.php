3c46c483a3a4f7f250c0dc8053dd7f7fa42bf5d2
FiveTwoFive <jabal@fivetwofive.com>
2021-12-01 10:42:34 -0800

    added GTM snippets and updated screenshots for themes

--- wp-content/themes/fivetwofive-theme/header.php
+++ wp-content/themes/fivetwofive-theme/header.php
@@ -16,6 +16,14 @@ $hide_site_description  = $fivetwofive_theme_mods['site_identity_hide_blogdescri
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-WV9BWXZ');</script>
+    <!-- End Google Tag Manager -->
+
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
@@ -24,6 +32,12 @@ $hide_site_description  = $fivetwofive_theme_mods['site_identity_hide_blogdescri
 </head>
 
 <body <?php body_class(); ?>>
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WV9BWXZ"
+                      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
 <?php wp_body_open(); ?>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'fivetwofive-theme' ); ?></a>
