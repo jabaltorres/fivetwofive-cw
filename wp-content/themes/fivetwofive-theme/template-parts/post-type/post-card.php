bc8a41d1c732f10597f4ae740f9b813f1927d597
FiveTwoFive <jabal@fivetwofive.com>
2022-10-07 09:39:41 -0700

    Wrapped post card with link to improve UX

--- wp-content/themes/fivetwofive-theme/template-parts/post-type/post-card.php
+++ wp-content/themes/fivetwofive-theme/template-parts/post-type/post-card.php
@@ -34,46 +34,48 @@ $post_categories = get_the_terms( $post_item_id, $post_taxonomy );
 ?>
 
 <article id="card-<?php echo esc_attr( $post_item_id ); ?>" <?php post_class( 'card', $post_item_id ); ?>>
-	<div class="card__top">
-		<?php if ( $post_categories ) : ?>
-			<ul class="card__categories">
-				<?php foreach ( $post_categories as $post_category ) : ?>
-					<li><a href="<?php echo esc_url( get_category_link( $post_category->term_id ) ); ?>"><?php echo esc_html( $post_category->name ); ?></a></li>
-				<?php endforeach; ?>
-			</ul>
-		<?php endif; ?>
+    <a href="<?php echo esc_url( get_permalink( $post_item_id ) ); ?>">
+        <div class="card__top">
+            <?php if ( $post_categories ) : ?>
+                <ul class="card__categories">
+                    <?php foreach ( $post_categories as $post_category ) : ?>
+                        <li><a href="<?php echo esc_url( get_category_link( $post_category->term_id ) ); ?>"><?php echo esc_html( $post_category->name ); ?></a></li>
+                    <?php endforeach; ?>
+                </ul>
+            <?php endif; ?>
 
-		<?php
-		echo get_the_post_thumbnail(
-			$post_item_id,
-			$image_size,
-			array(
-				'alt'   => the_title_attribute(
-					array(
-						'echo' => false,
-						'post' => $post_item_id,
-					)
-				),
-				'class' => 'card__image img-responsive',
-			)
-		);
-		?>
-	</div>
+            <?php
+            echo get_the_post_thumbnail(
+                $post_item_id,
+                $image_size,
+                array(
+                    'alt'   => the_title_attribute(
+                        array(
+                            'echo' => false,
+                            'post' => $post_item_id,
+                        )
+                    ),
+                    'class' => 'card__image img-responsive',
+                )
+            );
+            ?>
+        </div>
 
-	<div class="card__bottom">
-		<header class="card__header m-0">
-			<?php fivetwofive_theme_post_meta( $post_item_id ); ?>
-			<h3 class="card__title mt-2"><a href="<?php echo esc_url( get_permalink( $post_item_id ) ); ?>"><?php echo esc_html( get_the_title( $post_item_id ) ); ?></a></h3>
+        <div class="card__bottom">
+            <header class="card__header m-0">
+                <?php fivetwofive_theme_post_meta( $post_item_id ); ?>
+                <h3 class="card__title mt-2"><?php echo esc_html( get_the_title( $post_item_id ) ); ?></h3>
 
-			<?php if ( $post_tags_taxonomy ) : ?>
-				<?php echo get_the_term_list( get_the_ID(), $post_tags_taxonomy, '<p class="card__tags"><strong>Tags:</strong> ', ', ', '</p>' ); ?>
-			<?php endif; ?>
-		</header><!-- .card-header -->
+                <?php if ( $post_tags_taxonomy ) : ?>
+                    <?php echo get_the_term_list( get_the_ID(), $post_tags_taxonomy, '<p class="card__tags"><strong>Tags:</strong> ', ', ', '</p>' ); ?>
+                <?php endif; ?>
+            </header><!-- .card-header -->
 
-		<?php if ( has_excerpt( $post_item_id ) ) : ?>
-			<div class="card__content mt-2">
-				<?php echo wp_kses_post( get_the_excerpt( $post_item_id ) ); ?>
-			</div>
-		<?php endif; ?>
-	</div>
+            <?php if ( has_excerpt( $post_item_id ) ) : ?>
+                <div class="card__content mt-2">
+                    <?php echo wp_kses_post( get_the_excerpt( $post_item_id ) ); ?>
+                </div>
+            <?php endif; ?>
+        </div>
+    </a>
 </article><!-- #card-<?php echo esc_html( $post_item_id ); ?> -->
