73aa06830f50fb95dce83f91cd53f38533d1377a
FiveTwoFive <jabal@fivetwofive.com>
2023-06-07 15:00:56 -0700

    chore: deleted blocks plugin from repo

--- wp-content/plugins/fivetwofive-blocks/build/blocks/accordion/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '91d1aacf02fdcf0c72b1');
--- wp-content/plugins/fivetwofive-blocks/src/components/BlockTitle.js
+++
@@ -1,8 +0,0 @@
-
-const BlockTitle = ({ title }) => {
-	return (
-		<div className="ftfb-block-title">{title}</div>
-	);
-};
-
-export default BlockTitle;
