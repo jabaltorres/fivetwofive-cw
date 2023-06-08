c13026022c747f977449f474f6ab4c217911f86b
Danilo Parra Jr <daniloparrajr.work@gmail.com>
2023-06-08 16:32:34 +0800

    fix: update blog template

--- wp-content/themes/fivetwofive-theme/archive.php
+++ wp-content/themes/fivetwofive-theme/archive.php
@@ -11,31 +11,46 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
-
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
+		<header class="page-header">
+			<div class="container">
 				<?php
 				the_archive_title( '<h1 class="page-header__title">', '</h1>' );
 				the_archive_description( '<div class="page-header__description">', '</div>' );
 				?>
-			</header><!-- .page-header -->
+			</div>
+		</header><!-- .page-header -->
+
+		<?php
+		if ( have_posts() ) : ?>
+			<div class="row">
+
+				<?php
+				/* Start the Loop */
+				while ( have_posts() ) :
+					the_post();
+					?>
+
+					<div class="col-lg-4 col-md-6 col-sm-6 mb-3 mb-sm-4">
+						<?php
+						get_template_part(
+							'template-parts/post-type/post-card',
+							null,
+							array(
+								'id'       => get_the_ID(),
+								'taxonomy' => 'post_tag',
+							)
+						);
+						?>
+					</div>
+
+				<?php
+				endwhile;
+				?>
+			</div>
 
 			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
+			the_posts_pagination( array( 'mid_size'  => 2 ) );
 
 		else :
 
